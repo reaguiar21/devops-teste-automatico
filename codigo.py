cc807f9ff1d85f9ffc61ab9b56ece5b0ea397527
reaguiar21 <reaguiar@gmail.com>
2025-04-06 22:24:24 -0300

    Create codigo.py
    "Alteração no código de vendas"

---
+++ codigo.py
@@ -0,0 +1,11 @@
+vendas_mensais = [1000, 2000, 3000]
+
+def calcular_total_vendas(vendas):
+    return sum(vendas)
+
+def gerar_relatorio():
+    total = calcular_total_vendas(vendas_mensais)
+    print("Relatório de vendas - Março")
+    print("Total de vendas: R$", total)
+
+gerar_relatorio()
